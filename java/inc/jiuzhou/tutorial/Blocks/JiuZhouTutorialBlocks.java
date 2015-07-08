352c92ad3ce300239fba97350519611e9b242a72
PowerKG <137391258@qq.com>
2015-07-08 16:57:54 +0800

    修改文件路径【强迫症而已】

---
+++ java/inc/jiuzhou/tutorial/Blocks/JiuZhouTutorialBlocks.java
@@ -0,0 +1,14 @@
+package inc.jiuzhou.tutorial.blocks;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.material.Material;
+
+public class JiuZhouTutorialBlocks {
+	// 创建方块实例并且给赋值
+	public static final GuiBlock GuiBlock = new GuiBlock(Material.rock);
+
+	public static void init() {
+		/* 注册方块,到实战开发后我会提供好快捷注册库 */
+		GameRegistry.registerBlock(GuiBlock, GuiBlock.getUnlocalizedName());
+	}
+}
