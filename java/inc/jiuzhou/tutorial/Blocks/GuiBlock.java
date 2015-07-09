40fbdfa759a9d08d7626e31501e99ae63af24f69
PowerKG <137391258@qq.com>
2015-07-09 18:46:51 +0800

    完善明的补充内容

--- java/inc/jiuzhou/tutorial/Blocks/GuiBlock.java
+++ java/inc/jiuzhou/tutorial/Blocks/GuiBlock.java
@@ -1,19 +1,24 @@
-﻿package inc.jiuzhou.tutorial.blocks;
+﻿package inc.jiuzhou.tutorial.Blocks;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import inc.jiuzhou.tutorial.JiuzhouTutorial;
 import inc.jiuzhou.tutorial.BlockTileEntiies.GuiBlockTileEntity;
 import inc.jiuzhou.tutorial.Gui.GuiHandler;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
-/** 这是一个带有Gui的方块,因为可以放置东西合成什么的,所以这是一个继承于BlockContainer(方块容器)的类  **/
+/** 这是一个带有Gui的方块,因为可以放置东西合成什么的,所以这是一个继承于BlockContainer(方块容器)的类 **/
 public class GuiBlock extends BlockContainer {
-        //材质的设定
+	// 材质的设定
 	IIcon top = null, bottom = null, side1 = null, side2 = null, side3 = null, side4 = null;
+
 	public GuiBlock(Material 材质) {
 		/* 设置这个方块的材质(可以自定义材质: 是否可以被覆盖,燃烧,透过光线啥的),为了方便阅读，这个材质将会在注册的类里统一赋上相应的材质 */
 		super(材质);
@@ -21,13 +26,13 @@ public class GuiBlock extends BlockContainer {
 		this.setBlockName("GuiBlock");
 		/**
 		 * 1.7版本中setUnlocalizedName被改为setBlockName了,非1.7版本请用这个
-		*/
-		//this.setUnlocalizedName("GuiBlock");
+		 */
+		// this.setUnlocalizedName("GuiBlock");
 		/**
-		*简易设置材质方法(1.8不适用)
-		*/
-		//this.setBlockTextureName("GuiBlock");
-		
+		 * 简易设置材质方法(1.8不适用)
+		 */
+		// this.setBlockTextureName("GuiBlock");
+
 		/* 设置所在的创造栏的位置,这里设置的是tabBlock(方块类) */
 		this.setCreativeTab(CreativeTabs.tabBlock);
 		/*
@@ -59,42 +64,47 @@ public class GuiBlock extends BlockContainer {
 		entityPlayer.openGui(JiuzhouTutorial.实例, GuiHandler.GuiBlockGui, world, x, y, z);
 		return true;
 	}
-        /**以下内容为补充,暂可省略**/
+
+	/** 以下内容为补充,暂可省略 **/
 	/**
-	* 材质面获得方法(1.8同样不适用)
-	* 为什么这里要特別写呢,大槪因为本来的方法只返回1个值,所以我们要特別复盖方法
-	* @param face 方块面
-	* @param meta 方块数据值
-	* @return Icon
-	*/
+	 * 材质面获得方法(1.8同样不适用) 为什么这里要特別写呢,大槪因为本来的方法只返回1个值,所以我们要特別复盖方法
+	 * 
+	 * @param face
+	 *            方块面
+	 * @param meta
+	 *            方块数据值
+	 * @return Icon
+	 */
 	@SideOnly(Side.CLIENT)
 	@Override
-	public IIcon getIcon(int face, int meta){
-		switch(face){
-			case 0:
-				return top;
-			case 1:
-				return bottom;
-			case 2:
-				return side1;
-			case 3:
-				return side2;
-			case 4:
-				return side3;
-			case 5:
-				return side4;
-			default:
-				return top;
+	public IIcon getIcon(int face, int meta) {
+		switch (face) {
+		case 0:
+			return top;
+		case 1:
+			return bottom;
+		case 2:
+			return side1;
+		case 3:
+			return side2;
+		case 4:
+			return side3;
+		case 5:
+			return side4;
+		default:
+			return top;
 		}
 	}
+
 	/**
-	* 多材质设置方法(1.8同样不适用)
-	* 为什么这里要特別写呢,大槪因为本来的方法只会注册一个面,所以我们要特別复盖方法
-	* @param reg 注册器(不用理会)
-	*/
+	 * 多材质设置方法(1.8同样不适用) 为什么这里要特別写呢,大槪因为本来的方法只会注册一个面,所以我们要特別复盖方法
+	 * 
+	 * @param reg
+	 *            注册器(不用理会)
+	 */
 	@SideOnly(Side.CLIENT)
 	@Override
-	public void registerBlockIcons(IIconRegister reg){
+	public void registerBlockIcons(IIconRegister reg) {
 		this.top = reg.registerIcon("GuiBlock");
 		this.bottom = reg.registerIcon("GuiBlock");
 		this.side1 = reg.registerIcon("GuiBlock");
--- java/inc/jiuzhou/tutorial/Blocks/JiuZhouTutorialBlocks.java
+++ java/inc/jiuzhou/tutorial/Blocks/JiuZhouTutorialBlocks.java
@@ -1,4 +1,4 @@
-package inc.jiuzhou.tutorial.blocks;
+package inc.jiuzhou.tutorial.Blocks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.material.Material;
