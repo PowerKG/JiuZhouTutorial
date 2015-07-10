f08529cc082180aa1c579095087b74478c885853
PowerKG <137391258@qq.com>
2015-07-10 23:55:48 +0800

    给予TileEntity一个合成方法,并且描绘到GUI上

--- java/inc/jiuzhou/tutorial/BlockTileEntiies/GuiBlockTileEntity.java
+++ java/inc/jiuzhou/tutorial/BlockTileEntiies/GuiBlockTileEntity.java
@@ -1,5 +1,6 @@
 package inc.jiuzhou.tutorial.BlockTileEntiies;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
@@ -9,14 +10,99 @@ import net.minecraft.tileentity.TileEntity;
 public class GuiBlockTileEntity extends TileEntity implements IInventory {
 	/* 这个是容器内的现有的所有物品,[数字]所对应的物品为自己设置的 */
 	private ItemStack[] inventory;
+
 	/* 设置总共能装下多少物品 */
 	private int INVENTORY_SIZE;
 
+	/** 记录可以修复物品的最大耐久和目前耐久,若是-1则说明没有可以修复的物品 ,第三个变量则是用来校对是否替换了物品防止玩家刷修复 **/
+	public int maxDamage = -1, nowDamage = -1, beforeDamage = -1;
+
 	/* 初始化储存的ItemStack的inventory大小 */
 	public GuiBlockTileEntity() {
-		/*这里因为我的Gui只有两个自定义的格子,所以把大小设置为2,也可以直接赋值*/
+		/* 这里因为我的Gui只有两个自定义的格子,所以把大小设置为2,也可以直接赋值 */
 		INVENTORY_SIZE = 2;
 		inventory = new ItemStack[INVENTORY_SIZE];
+	}
+
+	/** 每当实体更新的时候 (每tick更新一次,1tick = 1/20s) **/
+	/**
+	 * 修复方法: 每tick时检测一下修复槽里是否有东西,有的话就判断是否能被修复,若能被修复就获取最大耐久,和现在耐久
+	 * 然后在GUI绘制修复进度,每tick修复1的耐久,当修复完成后,在右边槽放置修复好的,修复槽则设为空
+	 * **/
+	public void updateEntity() {
+		// 若修复槽为空则执行相应判断
+		if (inventory[0] == null) {
+			// 如果修复槽为空且变量没被重置的话将变量重置后退出
+			if (maxDamage != -1 || nowDamage != -1) {
+				maxDamage = -1;
+				nowDamage = -1;
+				beforeDamage = -1;
+			}
+			return;
+		} else {
+
+			// 判断成品槽是否有物品
+			if (inventory[1] != null)
+				return;
+
+		}
+		// 若没有在修复东西
+		if (maxDamage == -1 || nowDamage == -1) {
+			// 若该物品最大的耐久是0的话就说明这不是可以修复的工具
+			if (inventory[0].getMaxDamage() == 0)
+				return;
+			// 如果物品的当前耐久等于最大耐久就说明不用修复
+			if (inventory[0].getMaxDamage() == inventory[0].getMaxDamage() - inventory[0].getItemDamage())
+				return;
+			// 相应为数值赋值
+			maxDamage = inventory[0].getMaxDamage();
+			nowDamage = maxDamage - inventory[0].getItemDamage();
+			beforeDamage = inventory[0].getItemDamage();
+			// 执行本方法进行判断
+			updateEntity();
+			return;
+		} else {
+			// 校对是否更换了物品
+			if (inventory[0].getItemDamage() == beforeDamage ? inventory[0].getMaxDamage() == maxDamage : false) {
+				// 若通过校验则增加目前的修复量
+				++nowDamage;
+				// 判断是否修复完成
+				if (nowDamage == maxDamage) {
+					// 若修复完成则减少对应物品,然后在修复好的增加相应物品
+
+					// 复印这个物品且修复耐久
+					ItemStack item = inventory[0];
+					// 转移位置判断
+					if (inventory[0].stackSize - 1 == 0) {
+						// 若修复的物品数量只有一个则把该位置设置为空
+						inventory[0] = null;
+					} else {
+						// 若修复的物品数量不止一个则减去他的一个数量
+						--inventory[0].stackSize;
+					}
+
+					// 放置物品判断
+					if (inventory[1] == null) {
+						// 将之前已经修复好的复制品放进去
+						item.setItemDamage(0);
+						inventory[1] = item;
+					} else {
+						// 增加那个物品的数量
+						++inventory[1].stackSize;
+					}
+					return;
+				}
+			} else {
+				// 若玩家更换了物品则重置所有变量
+				maxDamage = -1;
+				nowDamage = -1;
+				beforeDamage = -1;
+				// 执行本方法重新赋值
+				updateEntity();
+			}
+			return;
+		}
+
 	}
 
 	// 下面都是乱七八糟的它自己补充的方法,现在可以先复制其他的进来
@@ -100,4 +186,11 @@ public class GuiBlockTileEntity extends TileEntity implements IInventory {
 		return INVENTORY_SIZE;
 	}
 
+	public boolean isRepairing() {
+		if (this.nowDamage != -1 && this.maxDamage != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- java/inc/jiuzhou/tutorial/Gui/Blocks/GuiGuiBlock.java
+++ java/inc/jiuzhou/tutorial/Gui/Blocks/GuiGuiBlock.java
@@ -13,6 +13,9 @@ public class GuiGuiBlock extends GuiContainer {
 	private static final ResourceLocation backgroundimage = new ResourceLocation("JiuZhouTutorial:" + "textures/gui/GuiBlock.png");
 	private GuiBlockTileEntity tile;
 
+	/* 绑定进度条的宽度和高度 */
+	private final int barWidth = 24, barHeight = 18;
+
 	/* 分别传入两个容器实例子,然后再用这两个容器创建一个新的的ContainerGuiBlock(GuiBlock的TileEntity容器)实例 */
 	public GuiGuiBlock(GuiBlockTileEntity GuiBlock的TileEntity容器, InventoryPlayer 玩家的背包容器, int 宽, int 高) {
 		super(new ContainerGuiBlock(GuiBlock的TileEntity容器, 玩家的背包容器));
@@ -22,10 +25,12 @@ public class GuiGuiBlock extends GuiContainer {
 		this.xSize = 宽;
 		this.ySize = 高;
 	}
-	/** 绘制前景,这里多会绘制文字*/
+
+	/** 绘制前景,这里多会绘制文字 */
 	@Override
-	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY){}
-	
+	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+	}
+
 	/** 绘制背景贴图 */
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int var1, int var2) {
@@ -33,6 +38,21 @@ public class GuiGuiBlock extends GuiContainer {
 		var1 = (this.width - xSize) / 2;
 		var2 = (this.height - ySize) / 2;
 		drawTexturedModalRect(var1, var2, 0, 0, xSize, ySize);
+		/* 绘制进度条 */
+		if (tile.isRepairing()) {
+			/** 绘制方法: 获得到总数和当前数,计算百分比,进度条X百分比得到需要绘制的长度,绘制到图中 **/
+			/** 用类似以上方法绘制出进度条 **/
+			// 总修复量: max-before 已经修复量: now - before
+			float all = tile.maxDamage - tile.beforeDamage, now = tile.nowDamage - tile.beforeDamage;
+			float percent = now / all;
+			// 如果为百分比为负数就不画
+			if (percent < 0) {
+			} else {
+				drawTexturedModalRect(var1 + 76, var2 + 20, 176, 0, (int) ((float) this.barWidth * percent), this.barHeight);
+
+			}
+		}
+		return;
 	}
 
 }
